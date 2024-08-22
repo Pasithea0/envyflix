b30f78530cab8283e8940e446adff2b0fc8c48c9
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-08-21 23:01:48 -0600

    add calc

---
+++ src/hooks/useCustomHue.ts
@@ -0,0 +1,36 @@
+const useCustomHex = "#000000";
+
+function hexToHue(hex: string) {
+  // Convert hex to RGB
+  const r = parseInt(hex.substring(1, 3), 16) / 255;
+  const g = parseInt(hex.substring(3, 5), 16) / 255;
+  const b = parseInt(hex.substring(5, 7), 16) / 255;
+
+  // Find min and max values
+  const min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+  const delta = max - min;
+
+  // Calculate Hue
+  let h;
+  if (delta === 0) {
+    h = 0;
+  } else if (max === r) {
+    h = (g - b) / delta;
+  } else if (max === g) {
+    h = 2 + (b - r) / delta;
+  } else {
+    h = 4 + (r - g) / delta;
+  }
+  h *= 60;
+  if (h < 0) {
+    h += 360;
+  }
+
+  return h;
+}
+
+export const useCustomHueValue = () => {
+  const hue = hexToHue(useCustomHex);
+  return hue;
+};
