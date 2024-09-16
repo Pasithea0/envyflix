33c7c171163cf97b608128bf1c4a5b4300147dc8
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-09-16 12:40:48 -0600

    hide chevrons on mobile

--- src/utils/discoverContent.tsx
+++ src/utils/discoverContent.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { get } from "@/backend/metadata/tmdb";
 import { Divider } from "@/components/utils/Divider";
 import { Flare } from "@/components/utils/Flare";
+import { useIsMobile } from "@/hooks/useIsMobile";
 import { conf } from "@/setup/config";
 import {
   Category,
@@ -112,6 +113,7 @@ export function DiscoverContent() {
   const gradientRef = useRef<HTMLDivElement>(null);
   const [countdownTimeout, setCountdownTimeout] =
     useState<NodeJS.Timeout | null>(null);
+  const { isMobile } = useIsMobile();
 
   const [editorPicksData, setEditorPicksData] = useState<Media[]>([]);
 
@@ -758,7 +760,7 @@ export function DiscoverContent() {
       <div className="mt-8 p-4 w-full max-w-screen-xl mx-auto">
         <h2 className="text-2xl font-bold mb-4 text-center">Movies:</h2>
         <div className="flex mb-4 overflow-x-auto">
-          <div className="">{renderScrollButton("movies", "left")}</div>
+          {!isMobile && <div>{renderScrollButton("movies", "left")}</div>}
           <div
             id="button-carousel-movies"
             className="flex mb-4 overflow-x-auto scroll-smooth"
@@ -772,12 +774,16 @@ export function DiscoverContent() {
               {renderMovieButtons()}
             </div>
           </div>
-          <div className="">{renderScrollButton("movies", "right")}</div>
+          {!isMobile && (
+            <div className="">{renderScrollButton("movies", "right")}</div>
+          )}
         </div>
 
         <h2 className="text-2xl font-bold mb-4 text-center">TV Shows:</h2>
         <div className="flex mb-4 overflow-x-auto">
-          <div className="">{renderScrollButton("tvshows", "left")}</div>
+          {!isMobile && (
+            <div className="">{renderScrollButton("tvshows", "left")}</div>
+          )}
           <div
             id="button-carousel-tvshows"
             className="flex mb-4 overflow-x-auto scroll-smooth"
@@ -791,7 +797,9 @@ export function DiscoverContent() {
               {renderTvButtons()}
             </div>
           </div>
-          <div className="">{renderScrollButton("tvshows", "right")}</div>
+          {!isMobile && (
+            <div className="">{renderScrollButton("tvshows", "right")}</div>
+          )}
         </div>
       </div>
       <div className="">
